8afad8ed685aa9572cf688995daa39b4ea4ce0e6
paulleonardo <pauleon@iu.edu>
2023-11-12 22:29:23 -0500

    Updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(){
         <>
         <nav className={styles.nav}>
             <ul className={styles.nav__linkHolder}>
-                <Link className={styles.nav__links} href='/home'>Home</Link>
+                <Link className={styles.nav__links} href='/'>Home</Link>
                 <Link className={styles.nav__links} href='/subscriptions'>Subscriptions</Link>
                 <Link className={styles.nav__links} href='/profile'>Profile</Link>
                 <Link className={styles.nav__links} href='/contact'>Contact</Link>
